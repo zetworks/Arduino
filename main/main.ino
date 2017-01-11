1562ef93efd1678f72b3eab381dd1cae8f2bcb25
zetworks <tzech@Thorstens-MacBook-Pro.local>
2017-01-11 15:02:42 +0100

    Master wird weiter entwickelt

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
 // hello 
+define <hhh.j>
+define <jjj.h>
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello World");
   Serial.println("This is a test");
+  erial.prinl("kkakks);
+  // das kommt wieder in den Master
 }
